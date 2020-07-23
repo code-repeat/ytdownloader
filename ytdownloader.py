d31343fcc72b0b57455e887ff0c6e24c0922caca
coder-repeat <coderepeat44@gmail.com>
2020-07-23 14:56:13 -0300

    Primeiro commit

---
+++ ytdownloader.py
@@ -0,0 +1,30 @@
+from colorama import Fore, init
+import os, webbrowser
+init()
+
+#  Imprime a capa do programa
+f = open('assets/fancyart.txt', 'r')
+show = ''.join(f.readlines())
+print(Fore.RED, show)
+
+#   Impede que o usuário deixe o campo de url vazio
+url = ''
+while url == '':
+    url = input('>>> URL: ')
+
+#   Verifica se a url é válida
+while url[:24] != 'https://www.youtube.com/':
+    print('url inválida!')
+    url = input('>>> URL: ')
+
+#  Fatia a string para adcionar o 'ss'
+url_slice1 = url[:12]
+url_slice2 = url[12:]
+
+#   Concatena as duas fatias, sendo que na primeira foi adcionado ss
+new_url = url_slice1 + 'ss' + url_slice2
+#   Abre uma nova aba do navegador, dierecionando o usuário para a página de download...
+webbrowser.open_new_tab(new_url)
+
+
+
